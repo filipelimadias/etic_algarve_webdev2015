d918a661bb70af2a587e38806e8b2f4f39766648
Antonio Alves <progmonc@gmail.com>
2015-02-02 21:56:14 +0000

    Comecei a fazer o ficheiro com o esqueleto base mas com muitas duvidas

--- vivendas.php
+++ vivendas.php
@@ -1,6 +1,58 @@
+
+
 <?php
 
-
-
+include 'dal_v2.php';
+include 'conexao.inc';
 
 ?>
+
+
+ <!DOCTYPE html>
+ 
+ <html>
+
+ <head>
+ 	<title>Ficha do Imovel</title>
+ </head>
+ <body>
+
+ <h3>Ficha de Imovel</h3>
+ 	<br>
+ 
+		 	<form action="" method="POST">
+		 		
+				<input type="id" name="Id" placeholder="Nome ">
+				<br>
+				<input type="ativo" name="Ativo" placeholder="Email ">
+				<br>
+				<input type="datainsert" name="datainsert" placeholder="Pass ">
+				<br>
+				<input type="idutilizador" name="nome" placeholder="Nome ">
+				<br>
+				<input type="nome" name="email" placeholder="Email ">
+				<br>
+				<input type="tipologia" name="pass" placeholder="Pass ">
+				<br>
+				<input type="distrito" name="nome" placeholder="Nome ">
+				<br>
+				<input type="concelho" name="email" placeholder="Email ">
+				<br>
+				<input type="localidade" name="pass" placeholder="Pass ">
+				<br>
+				<input type="latitude" name="pass" placeholder="Pass ">
+				<br>
+				<input type="longitude" name="pass" placeholder="Pass ">
+				<br>
+				<input type="morada" name="pass" placeholder="Pass ">
+				<br>
+				<input type="codigopostal" name="pass" placeholder="Pass ">
+				<br>
+				<input type="submit" name="submit"">
+		 
+		 	</form>
+		 	
+		 </body>
+ 
+
+ </html>
